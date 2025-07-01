a3e55b6bb875cd14b55949eb36da687ef819610c
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-01 22:29:30 +0530

    LARAVEL

--- 06_Form_Validation/app/Http/Controllers/AdvanceConrtoller.php
+++ 06_Form_Validation/app/Http/Controllers/AdvanceConrtoller.php
@@ -8,9 +8,15 @@ class AdvanceConrtoller extends Controller
 {
     function advanceValidate(Request $request){
         $request->validate([
-            "name"=>"required",
+            "name"=>"required | min:3 ",
             "email"=>"required",
-            "city"=>"required",
+            "city"=>"required|uppercase",
+        ],[
+            "name.required"=>"Plz Enter your Name",
+            "name.min"=>"the name atlest 3 word",
+            "name.array"=>"Plz Enter your Name in array form" ,
+            "email.required"=>"plz enter valid email",
+            "city.upercase"=>"plz enter city name is uppercase"
         ]);
         return $request;
     }
--- 06_Form_Validation/resources/views/AdvanceValidation.blade.php
+++ 06_Form_Validation/resources/views/AdvanceValidation.blade.php
@@ -2,32 +2,49 @@
     <h1>Add New User</h1>
     <form action="AddUser" method="post">
         @csrf
-        <input type="text" name="name" placeholder="Enter UserName">
-        <span>@error('name'){{$message}}
-        @enderror</span>
+        <input type="text" name="name" placeholder="Enter name" value="{{old("name")}}"
+        class="{{$errors->first("name") ? "error" : ""}}"
+        >
+        <span>
+            @error('name')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        <input type="text" name="email" placeholder="Enter UserName">
-        <span>@error("email"){{$message}}
-            
-        @enderror</span>
+        <input type="text" name="email" placeholder="Enter email" value="{{old("email")}}">
+        <span>
+            @error('email')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
-        <input type="text" name="city" placeholder="Enter UserName">
-        <span>@error('city'){{$message}}
-        @enderror</span>
+        <input type="text" name="city" placeholder="Enter city"
+        class="{{$errors->first("city")?"error": ""}}"
+        >
+        <span>
+            @error('city')
+                {{ $message }}
+            @enderror
+        </span>
         <br>
         <button>Add New User</button>
     </form>
 </div>
 <style>
- button ,input{
+    button,
+    input {
         height: 34px;
         width: 200px;
         text-align: center;
         margin: 4px
     }
-    button{
+
+    button {
         background-color: blue;
         color: white;
+    }
 
+    .error {
+        border: 5px solid red;
     }
 </style>
