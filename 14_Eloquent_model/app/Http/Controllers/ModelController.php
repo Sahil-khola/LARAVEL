c0aabe8d6b97a1118f1127a109a5ce9e5404e82e
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-05 23:17:10 +0530

    LARAVEL

--- 14_Eloquent_model/app/Http/Controllers/ModelController.php
+++ 14_Eloquent_model/app/Http/Controllers/ModelController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ModelController extends Controller
 {
-    //
+    function handelModel(){
+        $users = \App\Models\student::all();
+        $data = new \App\Models\student;
+        echo " <h1> {$data->getName()} </h1>";
+        // echo $data->getName();
+        return view("home",["users"=>$users]);
+        // return $users;
+    }
 }
--- 14_Eloquent_model/resources/views/home.blade.php
+++ 14_Eloquent_model/resources/views/home.blade.php
@@ -1,3 +1,26 @@
 <div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
+    <h1>HOME PAGE</h1>
 </div>
+
+
+
+<table border="2">
+    <thead>
+        <th>ID</th>
+        <th>Name</th>
+        {{-- <th>Email</th> --}}
+        <th>City</th>
+    </thead>
+    <tbody>
+        @foreach ($users as $user)
+            <tr>
+                {{-- <td> {{ $user->ID }}</td> --}}
+                <td> {{ $user->id }}</td>
+                {{-- <td> {{ $user->Name }}</td> --}}
+                <td> {{ $user->name }}</td>
+                {{-- <td> {{ $user->email }}</td> --}}
+                <td> {{ $user->city }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
