63fd4707cff318edf97fcb556951afa768fa54e0
Sahil-khola <blackiayaduvanshi@gmail.com>
2025-07-08 15:10:50 +0530

    LARAVEL

--- 14_Eloquent_model/routes/web.php
+++ 14_Eloquent_model/routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\ModelController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function() {
+    return view("welcome");
 });
-
-Route::get("/home",[ModelController::class,"handelModel"]);
